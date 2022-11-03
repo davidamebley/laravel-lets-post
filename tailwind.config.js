72390374e3e57fc5ce36a84e8ab2b35fc94dc7c9
David Amebley <davameb@gmail.com>
2022-11-03 21:42:51 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
