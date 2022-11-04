c789d99258d0207dda16c5915e95c21fb5c49ad8
David Amebley <davameb@gmail.com>
2022-11-04 16:49:08 +0200

    Create validation for Registration form

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,9 @@ use App\Http\Controllers\Auth\RegisterController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/posts', function () {
     return view('posts.index');
