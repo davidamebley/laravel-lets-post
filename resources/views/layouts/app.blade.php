26b1fc98e466a9eaa678b4df8b43922627e3e5c1
David Amebley <davameb@gmail.com>
2022-11-04 19:43:49 +0200

    Create logout function

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,10 @@
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li>
-                <a href="" class="p-3">Home</a>
+                <a href="/" class="p-3">Home</a>
             </li>
             <li>
-                <a href="" class="p-3">Dashboard</a>
+                <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
             </li>
             <li>
                 <a href="" class="p-3">Post</a>
@@ -27,7 +27,11 @@
                 <a href="" class="p-3">David Amebley</a>
             </li>
             <li>
-                <a href="" class="p-3">Log out</a>
+                <!-- We set up our logout route this way to prevent csrf -->
+                <form action="{{ route('logout') }}" method="post" class="p-3 inline">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
             </li>
             @endauth
 
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 
 /*
@@ -10,7 +11,13 @@ use App\Http\Controllers\Auth\RegisterController;
 | Web Routes
 |--------------------------------------------------------------------------
 */
+Route::get('/', function(){
+    return view('home');
+})->name('home');
+
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
