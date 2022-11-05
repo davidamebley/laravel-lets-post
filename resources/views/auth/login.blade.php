e98f268d44b745f0803d26dda7beab21a0805d58
David Amebley <davameb@gmail.com>
2022-11-05 23:52:20 +0200

    Fix login button labelling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,7 @@
                 </div>
 
                 <div>
-                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Log in</button>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Login</button>
                 </div>
 
             </form>
