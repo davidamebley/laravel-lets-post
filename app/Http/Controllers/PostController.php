07b39786f0385bda6979c29ded8c5287b82bde31
David Amebley <davameb@gmail.com>
2022-11-08 01:26:01 +0200

    Add auth middleware to Post a message feature to prevent unauth users

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware(['auth'])->only(['store', 'destroy']);    //We are giving exemptions to the functions in this array
+    }
+
     public function index(){
         // The 'with(['user', 'liks'])' snippet enables eager loading of the queries to prevent executing separe queries for the posts and the likes
         $posts = Post::latest()->with(['user', 'likes'])->paginate(20);   //Using pagination from Laravel
