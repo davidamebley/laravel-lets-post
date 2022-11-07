5703a9357ac2e272a6342ed566dbaa5c949719f7
David Amebley <davameb@gmail.com>
2022-11-08 01:06:19 +0200

    Add SoftDeletes to Likes table

--- database/migrations/2022_11_07_230146_add_soft_deletes_to_likes_table.php
+++ database/migrations/2022_11_07_230146_add_soft_deletes_to_likes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('likes', function (Blueprint $table) {
-            //
+            $table->softDeletes();
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('likes', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 };
