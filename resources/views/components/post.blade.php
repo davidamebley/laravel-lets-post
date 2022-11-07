0ea3f078d017116fcad7f0bf62772c1b8d75f1d3
David Amebley <davameb@gmail.com>
2022-11-07 20:02:43 +0200

    Create Posts Component

---
+++ resources/views/components/post.blade.php
@@ -0,0 +1,41 @@
+<div class="mb-4">
+                        <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
+
+                        <p class="mb-2">{{ $post->body }}</p>
+
+                        <!-- Prevent unauth users from accessing -->
+                        <!-- @auth -->
+                        <!-- Delete a Post -->
+                        @can('delete', $post)   <!-- Delete by Cheking our created Policy first -->
+                            <form action="{{ route('posts.destroy', $post) }}" method="post" class="mr-1">
+                                @csrf
+                                <!-- Use method spoofing here -->
+                                @method('DELETE')
+                                <button type="submit" class="text-blue-500">Delete</button>
+                            </form>
+                        @endcan
+                        <!-- @endauth -->
+
+                        <div class="flex items-center">
+                            <!-- Prevent unauth users from accessing -->
+                            @auth
+                                <!-- Check if post has been liked by user or not -->
+                                @if (!$post->likedBy(auth()->user()))
+                                    <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+                                        @csrf
+                                        <button type="submit" class="text-blue-500">Like</button>
+                                    </form>
+                                @else
+                                    <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+                                        @csrf
+                                        <!-- Use method spoofing here -->
+                                        @method('DELETE')
+                                        <button type="submit" class="text-blue-500">Unlike</button>
+                                    </form>
+                                @endif
+                            @endauth
+                            <!-- Add the Likes counter -->
+                            <span>{{ $post->likes->count() }}  {{ Str::plural('like', $post->likes->count()) }}</span>
+                        </div>
+
+                    </div>
