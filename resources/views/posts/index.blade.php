98ab6511b8c31230516f11bd402cab72b5761da2
David Amebley <davameb@gmail.com>
2022-11-07 14:58:44 +0200

    Fix unauth users viewing likes error

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -33,19 +33,23 @@
                         <p class="mb-2">{{ $post->body }}</p>
 
                         <div class="flex items-center">
-                            <!-- Check if post has been liked by user or not -->
-                            @if (!$post->likedBy(auth()->user()))
-                                <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
-                                    @csrf
-                                    <button type="submit" class="text-blue-500">Like</button>
-                                </form>
-                            @endif
-                                <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
-                                    @csrf
-                                    <!-- Use method spoofing here -->
-                                    @method('DELETE')
-                                    <button type="submit" class="text-blue-500">Unlike</button>
-                                </form>
+                            <!-- Prevent unauth users from accessing -->
+                            @auth
+                                <!-- Check if post has been liked by user or not -->
+                                @if (!$post->likedBy(auth()->user()))
+                                    <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+                                        @csrf
+                                        <button type="submit" class="text-blue-500">Like</button>
+                                    </form>
+                                @else
+                                    <form action="{{ route('posts.likes', $post->id) }}" method="post" class="mr-1">
+                                        @csrf
+                                        <!-- Use method spoofing here -->
+                                        @method('DELETE')
+                                        <button type="submit" class="text-blue-500">Unlike</button>
+                                    </form>
+                                @endif
+                            @endauth
                             <!-- Add the Likes counter -->
                             <span>{{ $post->likes->count() }}  {{ Str::plural('like', $post->likes->count()) }}</span>
                         </div>
