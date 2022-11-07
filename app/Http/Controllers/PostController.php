4ffaa6ee7f08139dababda6f8689cdee49728f43
David Amebley <davameb@gmail.com>
2022-11-07 14:50:04 +0200

    Apply Eager loading to the Post and Likes queries

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
-        $posts = Post::query()->paginate(20);   //Using pagination from Laravel
+        // The 'with(['user', 'liks'])' snippet enables eager loading of the queries to prevent executing separe queries for the posts and the likes
+        $posts = Post::with(['user', 'likes'])->paginate(20);   //Using pagination from Laravel
 
         return view('posts.index', [
             'posts' => $posts
