1bf712d2f15b3ae241951d4b7c2980851c0ae8e5
David Amebley <davameb@gmail.com>
2022-11-08 23:28:52 +0200

    Add some greeting text to Home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,20 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            Dashboard
+        <h1 class="text-2xl font-medium mb-4">Dashboard</h1>    
+        <p>Hi
+            @if (auth()->user())
+                {{auth()->user()->name}}.
+            @else
+                visitor.
+            @endif
+            We we're still building your dashboard.  What exciting ideas 
+            <br />do you have on what to see here? 
+        
+        <br /><br />
+        <span class="mt-20"> Share with us in the <a class="text-blue-500" href="{{ route('posts') }}" >Post</a> section!</span>
+
+        </p>
         </div>
     </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,19 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            Home
+        <h1 class="text-2xl font-medium mb-4">Home</h1>    
+        <p>Hi
+            @if (auth()->user())
+                {{auth()->user()->name}}.
+            @else
+                visitor.
+            @endif
+            Did you know that there is something big coming up concerning our Home page?
+        
+        <br /><br />
+        <span class="mt-20"> Visit the <a class="text-blue-500" href="{{ route('posts') }}" >Post</a> section and tell your friends how you feel.</span>
+
+        </p>
         </div>
     </div>
 @endsection
